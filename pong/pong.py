aeb22f265ac143c0666ca860a7562ff847a18d25
Paulo Sergio <psdsfj.snf20@uea.edu.br>
2021-05-15 13:23:03 -0400

    #Update: Draw canvas border and score

--- pong/pong.py
+++ pong/pong.py
@@ -1,144 +1,64 @@
-import os
 import turtle
 
 
-def paddle_1_up():
-    y = paddle_1.ycor()
-    if y < 250:
-        y += 30
-    else:
-        y = 250
-    paddle_1.sety(y)
-
-
-def paddle_1_down():
-    y = paddle_1.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_1.sety(y)
-
-
-def paddle_2_up():
-    y = paddle_2.ycor()
-    if y < 250:
-        y += 30
-    else:
-        y = 250
-    paddle_2.sety(y)
-
-
-def paddle_2_down():
-    y = paddle_2.ycor()
-    if y > -250:
-        y += -30
-    else:
-        y = -250
-    paddle_2.sety(y)
-
-
-if __name__ == '__main__':
-
+def create_screen():
     # draw screen
     screen = turtle.Screen()
     screen.title("My Pong")
     screen.bgcolor("black")
-    screen.setup(width=800, height=600)
-    screen.tracer(0)
+    screen.setup(width=1000, height=750)
 
-    # draw paddle 1
-    paddle_1 = turtle.Turtle()
-    paddle_1.speed(0)
-    paddle_1.shape("square")
-    paddle_1.color("white")
-    paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-    paddle_1.penup()
-    paddle_1.goto(-350, 0)
 
-    # draw paddle 2
-    paddle_2 = turtle.Turtle()
-    paddle_2.speed(0)
-    paddle_2.shape("square")
-    paddle_2.color("white")
-    paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-    paddle_2.penup()
-    paddle_2.goto(350, 0)
+def setup_turtle_border_pen():
+    turtle_border_pen.color("white")
+    turtle_border_pen.pensize(3)
+    turtle_border_pen.speed(6)
 
-    # draw ball
-    ball = turtle.Turtle()
-    ball.speed(0)
-    ball.shape("square")
-    ball.color("white")
-    ball.penup()
-    ball.goto(0, 0)
-    ball.dx = 1
-    ball.dy = 1
 
-    # score
-    score_1 = 0
-    score_2 = 0
+def move_turtle_border_pen_to_bottom_left():
+    turtle_border_pen.penup()
+    turtle_border_pen.hideturtle()
+    turtle_border_pen.goto(-500, -375)
+    turtle_border_pen.showturtle()
+    turtle_border_pen.pendown()
+
+
+def draw_canvas_border():
+    turtle_border_pen.forward(1000)
+    turtle_border_pen.left(90)
+    turtle_border_pen.forward(750)
+    turtle_border_pen.left(90)
+    turtle_border_pen.forward(1000)
+    turtle_border_pen.left(90)
+    turtle_border_pen.forward(750)
+    turtle_border_pen.hideturtle()
+
+
+def setup_turtle_hud_pen():
+    turtle_hud_pen.speed(0)
+    turtle_hud_pen.shape("square")
+    turtle_hud_pen.color("white")
+
+
+def move_turtle_hud_pen_and_draw_score():
+    turtle_hud_pen.penup()
+    turtle_hud_pen.hideturtle()
+    turtle_hud_pen.goto(0, 260)
+    turtle_hud_pen.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
+
+
+if __name__ == '__main__':
+    turtle_border_pen = turtle.Turtle()
+    turtle_hud_pen = turtle.Turtle()
+
+    create_screen()
+    setup_turtle_border_pen()
+    move_turtle_border_pen_to_bottom_left()
+    draw_canvas_border()
 
     # head-up display
-    hud = turtle.Turtle()
-    hud.speed(0)
-    hud.shape("square")
-    hud.color("white")
-    hud.penup()
-    hud.hideturtle()
-    hud.goto(0, 260)
-    hud.write("0 : 0", align="center", font=("Press Start 2P", 24, "normal"))
 
-    # keyboard
-    screen.listen()
-    screen.onkeypress(paddle_1_up, "w")
-    screen.onkeypress(paddle_1_down, "s")
-    screen.onkeypress(paddle_2_up, "Up")
-    screen.onkeypress(paddle_2_down, "Down")
+    setup_turtle_hud_pen()
+    move_turtle_hud_pen_and_draw_score()
 
-    while True:
-        screen.update()
-
-        # ball movement
-        ball.setx(ball.xcor() + ball.dx)
-        ball.sety(ball.ycor() + ball.dy)
-
-        # collision with the upper wall
-        if ball.ycor() > 290:
-            os.system("afplay bounce.wav&")
-            ball.sety(290)
-            ball.dy *= -1
-
-        # collision with lower wall
-        if ball.ycor() < -290:
-            os.system("afplay bounce.wav&")
-            ball.sety(-290)
-            ball.dy *= -1
-
-        # collision with left wall
-        if ball.xcor() < -390:
-            score_2 += 1
-            hud.clear()
-            hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-            os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
-            ball.goto(0, 0)
-            ball.dx *= -1
-
-        # collision with right wall
-        if ball.xcor() > 390:
-            score_1 += 1
-            hud.clear()
-            hud.write("{} : {}".format(score_1, score_2), align="center", font=("Press Start 2P", 24, "normal"))
-            os.system("afplay 258020__kodack__arcade-bleep-sound.wav&")
-            ball.goto(0, 0)
-            ball.dx *= -1
-
-        # collision with the paddle 1
-        if ball.xcor() < -330 and paddle_1.ycor() + 50 > ball.ycor() > paddle_1.ycor() - 50:
-            ball.dx *= -1
-            os.system("afplay bounce.wav&")
-
-        # collision with the paddle 2
-        if ball.xcor() > 330 and paddle_2.ycor() + 50 > ball.ycor() > paddle_2.ycor() - 50:
-            ball.dx *= -1
-            os.system("afplay bounce.wav&")
+    turtle.done()
